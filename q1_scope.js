fa5e5f4cd3b81ead475a413deb8581d41df5d11f
EKTA GUPTA <143394635+gupta-ekt@users.noreply.github.com>
2025-11-24 14:41:50 +0530

    ekta has updated the operator assignment

--- q1_scope.js
+++ q1_scope.js
@@ -1,17 +1,17 @@
 var bonus = 5000;
-var employeeIsPermanent = false;
+var employee = false;
 
 function calculateSalary() {
-  var baseSalary = 40000;
-  var localIsPermanent = true;
-  var totalSalary = baseSalary + (localIsPermanent ? bonus : 0);
+  var Salary = 40000;
+  var Permanent = true;
+  var totalSalary = Salary + (Permanent ? bonus : 0);
 
-  console.log("Inside function -> Permanent?", localIsPermanent);
-  console.log("Inside function -> Total Salary: ₹" + totalSalary);
+  console.log("Inside function -> Permanent?", Permanent);
+  console.log("Inside function -> Total Salary ₹" + totalSalary);
 }
 
-console.log("Global -> Permanent?", employeeIsPermanent);
+console.log("Global -> Permanent?", employee);
 
 calculateSalary();
 
-console.log("Global after function -> Permanent?", employeeIsPermanent);
+console.log("Global after function -> Permanent?", employee);
